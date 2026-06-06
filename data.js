64754c3d51d359417ad60d0464909983c4cb8f47
Aggressiv0 <160932463+Aggressiv0@users.noreply.github.com>
2026-06-06 12:34:50 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+export const data = {
+    videos: [
+        {
+            title: "Patukko talks about Orcas",
+            source: "https://www.youtube.com/embed/CRXZf2RWR-k?si=MNSwtL5khPlEbnTf&amp;start=4590",
+        },
+        {
+            title: "Patukkopia",
+            source: "https://www.youtube.com/embed/B8daRI602Ik?si=EArViHMGs6acX4xV"
+        }
+    ]
+};
